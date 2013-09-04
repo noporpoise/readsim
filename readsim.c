7a96839117e8a333fdeed4218f81b4f474e568ab
Isaac Turner <turner.isaac@gmail.com>
2013-09-04 17:50:22 +0100

    Pair reads with a random profile read to add sequencing errors

--- readsim.c
+++ readsim.c
@@ -92,77 +92,101 @@ char test_file_readable(const char *file)
   return 1;
 }
 
-void sum_qual_scores(long double *qprob_sum, size_t *counts, size_t len,
-                     const read_t *read, int offset)
+#define phred_to_prob(qual) powl(10, -(double)(qual)/10)
+
+void sum_qual_scores(const read_t *read, int qoffset,
+                     double *qprob_sum, size_t *counts, size_t len)
 {
   size_t i, limit = len < read->qual.end ? len : read->qual.end;
-  long double qual, qprob;
+  double qual;
+
   if(read->qual.end != read->seq.end) die("Invalid qual");
+
+  for(i = 0; i < read->qual.end; i++) {
+    if(read->qual.b[i] < qoffset) die("Invalid qual");
+    if(read->qual.b[i] - qoffset > 50) die("Invalid qual");
+    read->qual.b[i] -= qoffset;
+  }
+
   for(i = 0; i < limit; i++) {
-    if(read->qual.b[i] < offset) die("Invalid qual");
-    if(read->qual.b[i] - offset > 50) die("Invalid qual");
-    if(read->seq.b[i] != 'N') {
-      qual = read->qual.b[i] - offset;
-      qprob = powl(10, -qual/10);
-      qprob_sum[i] += qprob;
+    if(toupper(read->seq.b[i]) != 'N') {
+      qual = phred_to_prob(read->qual.b[i]);
+      qprob_sum[i] += qual;
       counts[i]++;
     }
   }
 }
 
-size_t generate_qprob(const char *path,
-                      long double **qprob_ptr, size_t **counts_ptr)
+// Returns length of first read
+size_t load_error_profile(const char *path, read_t **rptr, size_t *nreadsptr)
 {
   int qoffset, minq, maxq, fmt;
-  read_t r;
+
+  size_t ncap = 16;
+  read_t *r = malloc(ncap * sizeof(read_t));
 
   seq_file_t *file = seq_open(path);
-  seq_read_alloc(&r);
+  if(file == NULL) die("Cannot open file: %s", path);
 
   // Get FASTQ offset
   if((fmt = seq_guess_fastq_format(file, &minq, &maxq)) == -1)
     die("Cannot get quality score from: %s", path);
 
   qoffset = FASTQ_OFFSET[fmt];
+  printf("FASTQ offset: %i\n", qoffset);
 
-  if(seq_read(file, &r) <= 0)
+  if(!seq_read_alloc(&r[0])) die("Out of memory");
+  if(seq_read(file, &r[0]) <= 0)
     die("Empty profile file %s", path);
 
-  size_t i, len = r.seq.end, num_reads;
-  size_t *counts;
-  long double *qprob;
+  size_t i, len = r[0].seq.end, nreads;
+  size_t sumcount = 0, *counts;
+  double sumprob = 0, *qprob;
 
-  // qual_sum = calloc(len, sizeof(size_t));
+  qprob = malloc(len * sizeof(double));
   counts = calloc(len, sizeof(size_t));
-  qprob = malloc(len * sizeof(long double));
 
   if(counts == NULL || qprob == NULL) die("Out of memory");
 
-  for(i = 0; i < len; i++) qprob[i] = 0;
+  for(i = 0; i < len; i++) qprob[i] = 0.0;
 
-  sum_qual_scores(qprob, counts, len, &r, qoffset);
+  sum_qual_scores(&r[0], qoffset, qprob, counts, len);
 
-  for(num_reads = 1; seq_read(file, &r) > 0; num_reads++)
-    sum_qual_scores(qprob, counts, len, &r, qoffset);
-
-  seq_close(file);
-  seq_read_dealloc(&r);
-
-  printf("num profile reads: %zu\n", num_reads);
-
-  long double qtotal = 0;
-  for(i = 0; i < len; i++) {
-    qprob[i] /= counts[i];
-    qtotal += qprob[i];
+  for(nreads = 1; ; nreads++) {
+    if(nreads == ncap) { r = realloc(r, (ncap *= 2) * sizeof(read_t)); }
+    if(!seq_read_alloc(&r[nreads])) die("Out of memory");
+    if(seq_read(file, &r[nreads]) <= 0) { seq_read_dealloc(&r[nreads]); break; }
+    sum_qual_scores(&r[nreads], qoffset, qprob, counts, len);
   }
 
-  // Normalise
-  // long double qmean = qtotal / len, err_rate = 0.01;
+  printf("num profile reads: %zu\n", nreads);
+
+  // Normalise qprob
+  // double qmean = qtotal / len, err_rate = 0.01;
   // for(i = 0; i < len; i++)
   //   qprob[i] = err_rate * qprob[i] / qmean;
 
-  *qprob_ptr = qprob;
-  *counts_ptr = counts;
+  // Print qprob
+  printf("Counts: %zu", counts[0]);
+  sumcount = counts[0];
+  for(i = 1; i < len; i++) {
+    printf(",%zu", counts[i]);
+    sumcount += counts[i];
+  }
+  printf("\nCountsSum: %zu CountsMean: %zu\n", sumcount, sumcount / len);
+
+  printf("QProb: %f", qprob[0] / counts[0]);
+  for(i = 1; i < len; i++) {
+    printf(",%f", qprob[i] / counts[i]);
+    sumprob += qprob[i] / counts[i];
+  }
+  printf("\nQProbSum: %f QProbMean: %f\n", sumprob, sumprob / len);
+
+  free(qprob);
+  free(counts);
+
+  *rptr = r;
+  *nreadsptr = nreads;
 
   return len;
 }
@@ -211,39 +235,29 @@ static inline char mut(char c, int i)
   return bases[b+i];
 }
 
-void add_seq_error(char *r, size_t rlen,
-                   const long double *qprob, size_t *mcount, size_t errlen)
+void add_seq_error(char *seq, size_t seqlen, const read_t *r)
 {
-  size_t i, limit = rlen < errlen ? rlen : errlen;
+  size_t i, limit = seqlen < r->seq.end ? seqlen : r->seq.end;
   int rnd;
   for(i = 0; i < limit; i++) {
-    if((rnd = rand()) < qprob[i] * RAND_MAX) {
-      r[i] = mut(r[i], rnd % 3);
-      mcount[i]++;
-    }
+    if(r->seq.b[i] == 'N') seq[i] = 'N';
+    else if((rnd = rand()) < phred_to_prob(r->qual.b[i]) * RAND_MAX)
+      seq[i] = mut(seq[i], rnd % 3);
   }
 }
 
 // Load reads from a file, apply sequence error, dump
-void load_reads(const char *path, gzFile gzout,
-                const long double *qprob, size_t *mcount, size_t len)
+void mutate_reads(seq_file_t *sfile, gzFile gzout,
+                  const read_t *rlist, size_t nreads)
 {
-  seq_file_t *file;
   read_t r;
-  int i;
-
+  size_t rndread;
   seq_read_alloc(&r);
 
-  for(i = 0; i < 2; i++)
-  {
-    if((file = seq_open(path)) == NULL) die("Cannot read: %s", path);
-
-    while(seq_read(file, &r) > 0) {
-      add_seq_error(r.seq.b, r.seq.end, qprob, mcount, len);
-      gzprintf(gzout, "@%s\n%s\n+\n%s\n", r.name.b, r.seq.b, r.qual.b);
-    }
-
-    seq_close(file);
+  while(seq_read(sfile, &r) > 0) {
+    rndread = drand48() * (nreads-1);
+    add_seq_error(r.seq.b, r.seq.end, &rlist[rndread]);
+    gzprintf(gzout, "@%s\n%s\n+\n%s\n", r.name.b, r.seq.b, r.qual.b);
   }
 
   seq_read_dealloc(&r);
@@ -260,31 +274,28 @@ size_t rand_chrom(read_t *chroms, size_t nchroms, size_t totallen)
 }
 
 // Returns ref genome size
-size_t sim_reads(const char *refpath, gzFile out0, gzFile out1, long double *qprob,
-                 size_t *mcounts, size_t errlen, size_t tlen,
-                 size_t tlen_stddev, size_t rlen, double depth)
+size_t sim_reads(seq_file_t *reffile, gzFile out0, gzFile out1,
+                 const read_t *rlist, size_t rlistlen,
+                 size_t tlen, size_t tlen_stddev, size_t rlen, double depth)
 {
-  seq_file_t *file;
-  size_t i, rcap, nchroms, glen = 0, nreads, chr, pos0, pos1;
-  read_t *r;
+  size_t i, chromcap = 16, nchroms, glen = 0, nreads, chr, pos0, pos1;
+  read_t *chroms;
 
-  if((file = seq_open(refpath)) == NULL) die("Cannot read: %s", refpath);
+  chroms = malloc(chromcap * sizeof(read_t));
+  nchroms = 0;
 
-  rcap = 16;
-  r = malloc(rcap * sizeof(read_t));
-
-  for(i = 0; ; i++) {
-    if(i == rcap) {
-      rcap *= 2;
-      r = realloc(r, rcap * sizeof(read_t));
-    }
-    seq_read_alloc(&r[i]);
-    if(seq_read(file, &r[i]) <= 0) break;
-    glen += r[i].seq.end;
+  while(1)
+  {
+    if(nchroms == chromcap) chroms = realloc(chroms, (chromcap *= 2) * sizeof(read_t));
+    seq_read_alloc(&chroms[nchroms]);
+    if(seq_read(reffile, &chroms[nchroms]) <= 0) { seq_read_dealloc(&chroms[nchroms]); break; }
+    if(chroms[nchroms].seq.end < tlen) { seq_read_dealloc(&chroms[nchroms]); }
+    else glen += chroms[nchroms++].seq.end;
   }
-  nchroms = i;
 
-  if(nchroms == 0) die("No sequences in ref genome file: %s", refpath);
+  if(nchroms == 0)
+    die("No sequences long enough in ref genome file [min len: %zu]: %s",
+        tlen, reffile->path);
 
   // Sample
   nreads = (glen * depth) / (2 * rlen);
@@ -295,39 +306,27 @@ size_t sim_reads(const char *refpath, gzFile out0, gzFile out1, long double *qpr
 
   for(i = 0; i < nreads; i++)
   {
-    chr = (nchroms == 1) ? 0 : rand_chrom(r, nchroms, glen);
-    pos0 = drand48() * (r[chr].seq.end - tlen);
+    chr = (nchroms == 1) ? 0 : rand_chrom(chroms, nchroms, glen);
+    pos0 = drand48() * (chroms[chr].seq.end - tlen);
     pos1 = pos0 + tlen + ran_normal()*tlen_stddev - rlen;
-    if(pos1 + rlen > r[chr].seq.end) pos1 = r[chr].seq.end-rlen;
-    memcpy(read0, r[chr].seq.b+pos0, rlen);
-    memcpy(read1, r[chr].seq.b+pos1, rlen);
-    add_seq_error(read0, rlen, qprob, mcounts, errlen);
-    add_seq_error(read1, rlen, qprob, mcounts, errlen);
+    if(pos1 + rlen > chroms[chr].seq.end) pos1 = chroms[chr].seq.end-rlen;
+    memcpy(read0, chroms[chr].seq.b+pos0, rlen);
+    memcpy(read1, chroms[chr].seq.b+pos1, rlen);
+    add_seq_error(read0, rlen, &rlist[(int)(drand48() * (rlistlen-1))]);
+    add_seq_error(read1, rlen, &rlist[(int)(drand48() * (rlistlen-1))]);
     dna_revcmp(read1, rlen);
-    gzprintf(out0, ">r%zu:0:%s:%zu:%zu\n%.*s\n", i, r[chr].name.b, pos0, pos1, (int)rlen, read0);
-    gzprintf(out1, ">r%zu:1:%s:%zu:%zu\n%.*s\n", i, r[chr].name.b, pos0, pos1, (int)rlen, read1);
+    gzprintf(out0, ">r%zu:0:%s:%zu:%zu\n%.*s\n", i, chroms[chr].name.b, pos0, pos1, (int)rlen, read0);
+    gzprintf(out1, ">r%zu:1:%s:%zu:%zu\n%.*s\n", i, chroms[chr].name.b, pos0, pos1, (int)rlen, read1);
   }
 
-  seq_close(file);
-
-  for(i = 0; i < nchroms; i++) seq_read_dealloc(&r[i]);
-  free(r);
+  for(i = 0; i < nchroms; i++) seq_read_dealloc(&chroms[i]);
+  free(chroms);
 
   return glen;
 }
 
 int main(int argc, char **argv)
 {
-  // int k;
-  // double d = ran_normal()*5;
-  // printf("%f", d);
-  // for(k = 1; k < 5000; k++) {
-  //   d = ran_normal()*5;
-  //   printf(",%f", d);
-  // }
-  // printf("\n");
-  // exit(-1);
-
   if(argc < 4) print_usage(usage, NULL);
   srand(time(NULL) + getpid());
 
@@ -337,7 +336,7 @@ int main(int argc, char **argv)
   double depth = 1;
   int optr = 0, optt = 0, optv = 0, optl = 0, optd = 0; // keeps track of values
 
-  char *input0 = NULL, *input1 = NULL;
+  char *in0path = NULL, *in1path = NULL;
 
   int c;
   while((c = getopt(argc, argv, "r:t:v:l:d:1:2:")) >= 0) {
@@ -347,8 +346,8 @@ int main(int argc, char **argv)
       case 'v': tlen_stddev = atoi(optarg); optv++; break;
       case 'l': rlen = atoi(optarg); optl++; break;
       case 'd': depth = atol(optarg); optd++; break;
-      case '1': input0 = optarg; break;
-      case '2': input1 = optarg; break;
+      case '1': in0path = optarg; break;
+      case '2': in1path = optarg; break;
       default: die("Unknown option: %c", c);
     }
   }
@@ -361,17 +360,18 @@ int main(int argc, char **argv)
   if(optr > 1 || optt > 1 || optv > 1 || optl > 1 || optd > 1)
     print_usage(usage, "Duplicate args");
 
-  if((input0 == NULL) != (input1 == NULL))
+  if((in0path == NULL) != (in1path == NULL))
     print_usage(usage, "Need both -1 <in> -2 <in>");
 
   char *out0path = argv[optind+1], *out1path = argv[optind+2];  
-  gzFile out0 = NULL, out1 = NULL;
+  gzFile gzout0 = NULL, gzout1 = NULL;
+  seq_file_t *sf0 = NULL, *sf1 = NULL, *reffile = NULL;
 
-  if(input0 != NULL)
+  if(in0path != NULL)
   {
-    printf("Reading from %s and %s\n", input0, input1);
-    if(!test_file_readable(input0)) die("Cannot read: %s", input0);
-    if(!test_file_readable(input1)) die("Cannot read: %s", input1);
+    printf("Reading from %s and %s\n", in0path, in1path);
+    if((sf0 = seq_open(in0path)) == NULL) die("Cannot read: %s", in0path);
+    if((sf1 = seq_open(in1path)) == NULL) die("Cannot read: %s", in1path);
   }
 
   if(refpath != NULL)
@@ -381,66 +381,36 @@ int main(int argc, char **argv)
     printf(" template length: %i\n", tlen);
     printf(" template stddev: %i\n", tlen_stddev);
     printf(" sequencing depth: %f\n", depth);
-    if(!test_file_readable(refpath)) die("Cannot read: %s", refpath);
-    if((out0 = gzopen(out0path, "w")) == NULL) die("Cannot open: %s", out0path);
-    if((out1 = gzopen(out1path, "w")) == NULL) die("Cannot open: %s", out1path);
+    if((reffile = seq_open(refpath)) == NULL) die("Cannot read: %s", refpath);
+    if((gzout0 = gzopen(out0path, "w")) == NULL) die("Cannot open: %s", out0path);
+    if((gzout1 = gzopen(out1path, "w")) == NULL) die("Cannot open: %s", out1path);
   }
 
   // Generate profile from profile.fq
+  size_t errlen, glen, rlistlen = 0;
+  read_t *rlist;
 
-  // qprob[i] is the probability of making a seqn error at base i
-  long double *qprob, sumprob;
-  size_t i, errlen, *counts, sumcount, glen;
+  errlen = load_error_profile(argv[optind], &rlist, &rlistlen);
 
-  errlen = generate_qprob(argv[optind], &qprob, &counts);
-
-  // Print qprob
-  printf("QProb: %Lf", qprob[0]);
-  sumprob = qprob[0];
-  for(i = 1; i < errlen; i++) {
-    printf(",%Lf", qprob[i]);
-    sumprob += qprob[i];
-  }
-  printf("\nQProbSum: %Lf QProbMean: %Lf\n", sumprob, sumprob / errlen);
-
-  printf("Counts: %zu", counts[0]);
-  sumcount = counts[0];
-  for(i = 1; i < errlen; i++) {
-    printf(",%zu", counts[i]);
-    sumcount += counts[i];
-  }
-  printf("\nCountsSum: %zu CountsMean: %zu\n", sumcount, sumcount / errlen);
-
-  // Reuse mcounts for recording distribution of mutations
-  memset(counts, 0, sizeof(*counts) * errlen);
-
-  if(input0 != NULL)
-  {
-    load_reads(input0, out0, qprob, counts, errlen);
-    load_reads(input1, out1, qprob, counts, errlen);
+  if(sf0 != NULL) {
+    mutate_reads(sf0, gzout0, rlist, rlistlen);
+    mutate_reads(sf1, gzout1, rlist, rlistlen);
+    seq_close(sf0);
+    seq_close(sf1);
   }
 
   if(refpath != NULL) {
-    glen = sim_reads(refpath, out0, out1, qprob, counts, errlen,
+    glen = sim_reads(reffile, gzout0, gzout1, rlist, rlistlen,
                      tlen, tlen_stddev, rlen, depth);
+    seq_close(reffile);
   }
 
-  if(out0 != NULL) {
-    gzclose(out0);
-    gzclose(out1);
-
-    printf("SeqErr: %zu", counts[0]);
-    sumcount = counts[0];
-    for(i = 1; i < errlen; i++) {
-      printf(",%zu", counts[i]);
-      sumcount += counts[i];
-    }
-    printf("\nSeqErrSum: %zu SeqErrMean: %f\n",
-           sumcount, (double)sumcount / (glen*depth));
+  if(gzout0 != NULL) {
+    gzclose(gzout0);
+    gzclose(gzout1);
   }
 
-  free(qprob);
-  free(counts);
+  free(rlist);
 
   return EXIT_SUCCESS;
 }
